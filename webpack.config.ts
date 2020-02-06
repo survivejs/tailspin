6ae00c11f0009ddc482f8b95fe7ba793909445fe
Juho Vepsäläinen <bebraw@gmail.com>
2020-02-06 10:58:33 +0100

    chore: Set up entry plugin

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,14 +1,22 @@
 import path from "path";
 import webpack from "webpack";
+import WebpackWatchedGlobEntries from "webpack-watched-glob-entries-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import merge from "webpack-merge";
 
 const ROOT = __dirname;
 const PATHS = {
   SRC: path.resolve(ROOT, "src"),
+  OUTPUT: path.resolve(ROOT, "public"),
 };
 
 const commonConfig: webpack.Configuration = {
+  entry: WebpackWatchedGlobEntries.getEntries([
+    path.resolve(PATHS.SRC, "**/*.js"),
+  ]),
+  output: {
+    path: PATHS.OUTPUT,
+  },
   module: {
     rules: [
       {
@@ -24,20 +32,21 @@ const commonConfig: webpack.Configuration = {
       },
     ],
   },
+  plugins: [new WebpackWatchedGlobEntries()],
 };
 
 const developmentConfig: webpack.Configuration = {};
 const productionConfig: webpack.Configuration = {};
 
-export default env => {
-  switch (env) {
+export default mode => {
+  switch (mode) {
     case "development": {
-      return merge(commonConfig, developmentConfig);
+      return merge(commonConfig, developmentConfig, { mode });
     }
     case "production": {
-      return merge(commonConfig, productionConfig);
+      return merge(commonConfig, productionConfig, { mode });
     }
   }
 
-  throw new Error(`Unknown target: ${env}`);
+  throw new Error(`Unknown target: ${mode}`);
 };
