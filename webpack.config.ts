4933bde4d02b3f63311a55bf5d05f8343b7d09d7
Juho Vepsäläinen <bebraw@gmail.com>
2020-02-06 10:47:41 +0100

    chore: Add initial webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,5 +1,43 @@
-export default env => {
-  console.log("hello from webpack config", env);
+import path from "path";
+import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import merge from "webpack-merge";
 
-  return {};
+const ROOT = __dirname;
+const PATHS = {
+  SRC: path.resolve(ROOT, "src"),
+};
+
+const commonConfig: webpack.Configuration = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: PATHS.SRC,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(css|pcss)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+};
+
+const developmentConfig: webpack.Configuration = {};
+const productionConfig: webpack.Configuration = {};
+
+export default env => {
+  switch (env) {
+    case "development": {
+      return merge(commonConfig, developmentConfig);
+    }
+    case "production": {
+      return merge(commonConfig, productionConfig);
+    }
+  }
+
+  throw new Error(`Unknown target: ${env}`);
 };
