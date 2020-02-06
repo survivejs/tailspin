5f1f6e7aa7b3bb26789c9804b0a2c9a9dd9bb98d
Juho Vepsäläinen <bebraw@gmail.com>
2020-02-06 11:06:48 +0100

    chore: Set up plugins

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,11 +3,13 @@ import webpack from "webpack";
 import WebpackWatchedGlobEntries from "webpack-watched-glob-entries-plugin";
 import { WebpackPluginServe } from "webpack-plugin-serve";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import CopyPlugin from "copy-webpack-plugin";
 import merge from "webpack-merge";
 
 const PORT = 8080;
 const ROOT = __dirname;
 const PATHS = {
+  ASSETS: path.resolve(ROOT, "assets"),
   SRC: path.resolve(ROOT, "src"),
   OUTPUT: path.resolve(ROOT, "public"),
 };
@@ -34,7 +36,13 @@ const commonConfig: webpack.Configuration = {
       },
     ],
   },
-  plugins: [new WebpackWatchedGlobEntries()],
+  plugins: [
+    new WebpackWatchedGlobEntries(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new CopyPlugin([{ from: PATHS.ASSETS, to: "assets" }]),
+  ],
 };
 
 const developmentConfig: webpack.Configuration = {
