5fba7f91d3314198ac90b702fb4435a98955e1c7
Juho Vepsäläinen <bebraw@gmail.com>
2020-02-06 15:06:13 +0100

    chore: Sketch out a sample page
    This needs to be loaded through a plugin still.

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,21 @@
 import * as elements from "typed-html";
+import Page from "./layouts/page";
 
 export default (
-  <main>
-    <h1>tailwind-webpack-starter</h1>
+  <Page
+    head={[
+      <title>tailwind-webpack-starter</title>,
+      <meta
+        name="description"
+        content="tailwind-webpack-starter combines webpack with Tailwind and provides a starting point for site projects"
+      ></meta>,
+    ]}
+    body={
+      <main>
+        <h1>tailwind-webpack-starter</h1>
 
-    <p>TODO</p>
-  </main>
+        <p>TODO</p>
+      </main>
+    }
+  />
 );
---
+++ src/layouts/page.tsx
@@ -0,0 +1,8 @@
+import * as elements from "typed-html";
+
+export default ({ head, body }) => (
+  <html lang="en">
+    <head>{head}</head>
+    <body>{body}</body>
+  </html>
+);
