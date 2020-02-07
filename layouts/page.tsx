2710390372b8d944619cee501ddb7951b0a00007
Juho Vepsäläinen <bebraw@gmail.com>
2020-02-07 15:12:56 +0100

    fix: Fix scaling on mobile

--- layouts/page.tsx
+++ layouts/page.tsx
@@ -3,6 +3,7 @@ import * as elements from "typed-html";
 export default ({ head, body, cssTags, jsTags, htmlAttributes }) => (
   <html {...htmlAttributes}>
     <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       {head}
       {cssTags}
     </head>
