06362b0a5fdb072ef826df2448785df05e29b5d2
Juho Vepsäläinen <bebraw@gmail.com>
2020-05-12 16:57:22 +0300

    chore: Add initial fixes

---
+++ @types/typed-html.d.ts
@@ -0,0 +1,5 @@
+declare namespace JSX {
+  interface HtmlTag {
+    x?: string;
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import * as elements from "typed-html";
 import Page from "./_layouts/page";
 import Button from "./_components/button";
 
-// TODO: Figure out how to inject sidewind attributes.
-// Maybe attribute naming has to change?
 export default ({ htmlAttributes, cssTags, jsTags }) => (
   <Page
     htmlAttributes={htmlAttributes}
@@ -21,7 +19,7 @@ export default ({ htmlAttributes, cssTags, jsTags }) => (
         <h1>tailwind-webpack-starter</h1>
         <p>TODO</p>
         <div x-state="false">
-          Value: <span x-value="state" />
+          Value: <span x="state" />
         </div>
         <Button label="Demo button" />
       </main>
