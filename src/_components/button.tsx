440bf43051c8aa5164a56641b67bda77ae6c61bd
Juho Vepsäläinen <bebraw@gmail.com>
2020-07-07 10:25:31 +0200

    chore: Link to buttons

--- src/_components/button.tsx
+++ src/_components/button.tsx
@@ -1,5 +1,6 @@
 import * as elements from "typed-html";
 
+// https://tailwindcss.com/components/buttons
 export default ({ onclick }, label) => (
   <button class="btn btn-blue" onclick={onclick}>
     {label}
