c80dd479d9aeea7b908fe25790cd7be38989a89f
Juho Vepsäläinen <bebraw@gmail.com>
2020-07-26 17:12:48 +0200

    chore: Improve rendering
    Likely this bit could go to the base template.

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -18,45 +18,47 @@ export default ({ htmlAttributes, cssTags, jsTags }) => (
     ]}
     body={
       <main>
-        <h1>Available components</h1>
+        <article class="w-full mx-auto prose lg:prose-xl">
+          <h1>Available components</h1>
 
-        <div class="mb-4">
-          <h2>Alert</h2>
-          <Alert>This is a demo alert</Alert>
-        </div>
+          <div class="mb-4">
+            <h2>Alert</h2>
+            <Alert>This is a demo alert</Alert>
+          </div>
 
-        <div class="mb-4">
-          <h2>Button</h2>
-          <div x-state="false">
-            <div class="mb-4">
-              Value: <span x="state" />
-            </div>
-            <div>
-              <Button onclick="setState(v => !v)">Demo button</Button>
+          <div class="mb-4">
+            <h2>Button</h2>
+            <div x-state="false">
+              <div class="mb-4">
+                Value: <span x="state" />
+              </div>
+              <div>
+                <Button onclick="setState(v => !v)">Demo button</Button>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div>
-          <h2>Navigation</h2>
-          <Navigation>
-            <div class="text-sm lg:flex-grow">
-              <NavigationItem href="/">Documentation</NavigationItem>
-              <NavigationItem href="/components">Components</NavigationItem>
-            </div>
-            <div>
-              <NavigationItem href="https://github.com/survivejs/tailwind-webpack-starter">
-                Download
-              </NavigationItem>
-              <NavigationItem
-                href="https://github.com/survivejs/tailwind-webpack-starter"
-                cls="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white"
-              >
-                Download
-              </NavigationItem>
-            </div>
-          </Navigation>
-        </div>
+          <div>
+            <h2>Navigation</h2>
+            <Navigation>
+              <div class="text-sm lg:flex-grow">
+                <NavigationItem href="/">Documentation</NavigationItem>
+                <NavigationItem href="/components">Components</NavigationItem>
+              </div>
+              <div>
+                <NavigationItem href="https://github.com/survivejs/tailwind-webpack-starter">
+                  Download
+                </NavigationItem>
+                <NavigationItem
+                  href="https://github.com/survivejs/tailwind-webpack-starter"
+                  cls="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white"
+                >
+                  Download
+                </NavigationItem>
+              </div>
+            </Navigation>
+          </div>
+        </article>
       </main>
     }
   />
