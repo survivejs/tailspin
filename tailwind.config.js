f8c138c99a796fdc827e7df5bbecf6b7a49d6636
Juho Vepsäläinen <bebraw@gmail.com>
2020-07-27 18:21:04 +0200

    fix: Re-enable prose
    Purge configuration needs to be a bit different for this case. Now it's
    20k CSS but that's not too bad.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@ module.exports = {
   theme: {},
   variants: {},
   corePlugins: {},
-  // https://github.com/tailwindlabs/tailwindcss-typography/issues/35
-  // plugins: [require("@tailwindcss/typography")],
-  purge: ["./src/**/*.ts", "./src/**/*.tsx"],
+  plugins: [require("@tailwindcss/typography")],
+  purge: {
+    mode: "all",
+    content: ["./src/**/*.ts", "./src/**/*.tsx"],
+  },
 };
