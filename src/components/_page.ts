e78700ac7c107080a70b74bbe8f81567262a2771
Juho Vepsäläinen <bebraw@gmail.com>
2020-08-08 14:04:57 +0200

    chore: Add a demo for rendering a heading

--- src/components/_page.ts
+++ src/components/_page.ts
@@ -1,3 +1,8 @@
+import Heading from "../_primitives/heading";
+
+// TODO: detect heading by name and create it like this
+console.log(Heading({ as: "h4" }, "demo header"));
+
 // @ts-ignore: TODO: Add this to global
 window.evaluateCode = (code) => {
   // TODO: Evaluate all nodes through patterns/primitives
