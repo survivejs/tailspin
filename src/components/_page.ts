bfddf1ec5ede21b3f585ff09e01d3c57c08d1ecf
Juho Vepsäläinen <bebraw@gmail.com>
2020-08-08 14:27:26 +0200

    chore: Render Link correctly

--- src/components/_page.ts
+++ src/components/_page.ts
@@ -1,17 +1,26 @@
 import Heading from "../_primitives/heading";
+import Link from "../_primitives/link";
 
 // @ts-ignore: TODO: Add this to global
 window.evaluateCode = (code) => {
   // TODO: Evaluate all nodes through patterns/primitives
-  const componentNode = new DOMParser().parseFromString(code, "text/html").body
+  const componentNode = new DOMParser().parseFromString(code, "text/xml")
     .firstElementChild;
 
   if (!componentNode) {
     return "";
   }
 
-  if (componentNode.nodeName === "HEADING") {
+  if (componentNode.nodeName === "Heading") {
     return Heading(
+      // @ts-ignore: Evaluated runtime
+      attributesToObject(componentNode.attributes),
+      componentNode.innerHTML
+    );
+  }
+
+  if (componentNode.nodeName === "Link") {
+    return Link(
       // @ts-ignore: Evaluated runtime
       attributesToObject(componentNode.attributes),
       componentNode.innerHTML
