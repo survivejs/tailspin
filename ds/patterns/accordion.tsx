74eebc2dfcd2df972b4357434a23a4858c3dd187
Juho Vepsäläinen <bebraw@gmail.com>
2020-08-11 10:53:40 +0200

    feat: Add Accordion pattern

---
+++ ds/patterns/accordion.tsx
@@ -0,0 +1,31 @@
+import * as elements from "typed-html";
+import Box from "../primitives/box";
+import Flex from "../primitives/flex";
+
+// https://tailwindcss.com/components/alerts
+const Accordion = ({ title }: { title: string }, children: string[]) => (
+  <Box mb="2" x-state="false">
+    <Flex
+      direction="row"
+      sx="justify-between cursor-pointer"
+      onclick="setState(visible => !visible)"
+    >
+      <Box as="span">{title}</Box>
+      <Box as="span" x="state ? '+' : '-'" />
+    </Flex>
+    <div class="py-2 text-gray-600" x-class="!state && 'hidden'">
+      {children.join("")}
+    </div>
+  </Box>
+);
+
+export const description =
+  "Use Accordion when you want to show a lot of information in a compact space.";
+export const displayName = "Accordion";
+export const Example = () => (
+  <Accordion title="Junior engineer">
+    Junior engineer description goes here
+  </Accordion>
+);
+
+export default Accordion;
