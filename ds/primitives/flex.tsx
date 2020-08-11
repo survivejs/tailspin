1f1bb96c29215467dccb52f8401c2ff717923a6d
Juho Vepsäläinen <bebraw@gmail.com>
2020-08-11 10:13:37 +0200

    feat(flex): Add initial support for direction

--- ds/primitives/flex.tsx
+++ ds/primitives/flex.tsx
@@ -1,16 +1,29 @@
 import * as elements from "typed-html";
+import omit from "object.omit";
 import Box, { BoxProps } from "./box";
 
 // https://theme-ui.com/components/flex
-const Flex = (props: BoxProps = {}, children: string[]) => (
-  <Box {...props} sx={`flex ${(props?.sx && props.sx) || ""}`.trim()}>
+const Flex = (
+  props: BoxProps & { direction?: "column" | "row" } = { direction: "column" },
+  children: string[]
+) => (
+  <Box
+    {...props}
+    sx={`flex ${parseDirectionClass(props?.direction)} ${
+      (props?.sx && omit(props.sx, "direction")) || ""
+    }`.trim()}
+  >
     {children.join("")}
   </Box>
 );
 
+function parseDirectionClass(direction) {
+  return direction === "column" ? "flex-col" : "flex-row";
+}
+
 export const displayName = "Flex";
 export const Example = () => (
-  <Flex>
+  <Flex direction="column">
     <Box p="2" bg="primary" sx="flex-auto">
       Flex
     </Box>
