1755538c9af0613f0ae2d510db6d29415eb6af61
Juho Vepsäläinen <bebraw@gmail.com>
2020-08-12 13:53:48 +0200

    refactor: Co-locate test components

--- pages/design-system/evaluate-jsx.test.tsx
+++ pages/design-system/evaluate-jsx.test.tsx
@@ -1,26 +1,14 @@
 import * as elements from "typed-html";
 import evaluateJSX from "./evaluate-jsx";
 
-const ShowChildren = ({}, children: string[]) => <div>{children}</div>;
-const PassProps = ({ test }: { test: string }, children: string[]) => (
-  <div>{test}</div>
-);
-const PassChildrenAndProps = (
-  { test }: { test: string },
-  children: string[]
-) => (
-  <div>
-    {test}
-    {children}
-  </div>
-);
-
 describe("JSX evaluator", () => {
   test("should return empty string for empty string", () => {
     expect(evaluateJSX("", {})).toBe("");
   });
 
   test("should evaluate a component", () => {
+    const ShowChildren = ({}, children: string[]) => <div>{children}</div>;
+
     expect(
       evaluateJSX("<ShowChildren>test</ShowChildren>", {
         ShowChildren,
@@ -29,6 +17,10 @@ describe("JSX evaluator", () => {
   });
 
   test("should evaluate component props", () => {
+    const PassProps = ({ test }: { test: string }, children: string[]) => (
+      <div>{test}</div>
+    );
+
     expect(
       evaluateJSX(`<PassProps test="test" />`, {
         PassProps,
@@ -37,6 +29,16 @@ describe("JSX evaluator", () => {
   });
 
   test("should evaluate component children and props", () => {
+    const PassChildrenAndProps = (
+      { test }: { test: string },
+      children: string[]
+    ) => (
+      <div>
+        {test}
+        {children}
+      </div>
+    );
+
     expect(
       evaluateJSX(
         `<PassChildrenAndProps test="prop">children</PassChildrenAndProps>`,
