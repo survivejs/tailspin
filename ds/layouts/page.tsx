1c354f187156755caa1a4d019cd3f1bd1716a76c
Juho Vepsäläinen <bebraw@gmail.com>
2020-08-16 14:32:54 +0200

    fix: Use the same font for code editor elements

--- ds/layouts/page.tsx
+++ ds/layouts/page.tsx
@@ -10,6 +10,10 @@ const PageLayout = ({ head, body, cssTags, jsTags, htmlAttributes, url }) => (
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       {head}
       {cssTags}
+      <link
+        href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&display=swap"
+        rel="stylesheet"
+      ></link>
     </head>
     <body>
       <Box as="header" bg="primary" color="white">
