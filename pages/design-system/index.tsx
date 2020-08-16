3314f0d98d7a71c2cf748e7bbf5952fcbe3a5776
Juho Vepsäläinen <bebraw@gmail.com>
2020-08-16 10:51:47 +0200

    chore: Add code to debug

--- pages/design-system/index.tsx
+++ pages/design-system/index.tsx
@@ -331,6 +331,7 @@ const Collection = ({ items }) =>
                 )}
               </Tab.Body>
             </Tab.Container>
+            <Box x="codeEditor.exampleSource" />
             <Box onclick="setState({ exampleSource: 'demo' }, { parent: 'codeEditor' })">
               change example source
             </Box>
