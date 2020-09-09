13503d5d98c6f01766ce412dff84f041aee3079d
Juho Vepsäläinen <bebraw@gmail.com>
2020-09-09 16:52:39 +0200

    chore: Add stitches demo

--- src/demo.tsx
+++ src/demo.tsx
@@ -1,6 +1,25 @@
 import * as elements from "./elements.ts";
 import Box from "../ds/primitives/box.tsx";
 
+/*
+import css from "https://dev.jspm.io/@stitches/css@9.0.0-alpha.8";
+
+console.log(typeof window, window);
+
+const c = css.createCss({ prefix: "", tokens: {}, breakpoints: {}, utils: {} });
+
+const button = c({
+  color: "gray",
+  "&:hover": {
+    color: "black",
+  },
+  borderColor: "black",
+  padding: "1rem",
+});
+
+console.log("css", c);
+*/
+
 export const hello = () => (
   <Box m="2" p="2" color="white" bg="primary">
     hello world
