f0a17a94f64a73618185c4815f77490ef75fe664
Juho Vepsäläinen <bebraw@gmail.com>
2020-09-17 15:33:44 +0200

    chore: Add a test stub

---
+++ pages/design-system/query-nodes.test.ts
@@ -0,0 +1,9 @@
+import {
+  assertEquals,
+} from "https://deno.land/std@0.69.0/testing/asserts.ts";
+
+// TODO: Add proper tests for query nodes ops
+Deno.test("hello world #1", () => {
+  const x = 1 + 2;
+  assertEquals(x, 3);
+});
