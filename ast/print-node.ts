737a8b489b4b769779fb4eee723f7efc6f0f66bb
Juho Vepsäläinen <bebraw@gmail.com>
2020-09-18 10:55:35 +0200

    chore: Drop print node helper
    This isn't needed anymore.

--- ast/print-node.ts
+++
@@ -1,8 +0,0 @@
-import walkAst from "./walk-ast.ts";
-import { AstNode } from "../types.ts";
-
-function printNodes(node: AstNode) {
-  walkAst({ node, onNode: (node: AstNode) => console.log(node) });
-}
-
-export default printNodes;
--- ast/to-source.test.ts
+++ ast/to-source.test.ts
@@ -1,14 +1,13 @@
 import { assertEquals } from "../deps.ts";
 import queryNodes from "./query-nodes.ts";
 import toSource from "./to-source.ts";
-import printNode from "./print-node.ts";
 
 Deno.test("prints component source", () => {
   const source = "const Demo = () => <div>demo</div>;";
   const type = "VariableDeclaration";
   const node = queryNodes({ source, query: { type } })[0];
 
-  // console.log(printNode(node));
+  // console.log(JSON.stringify(node, null, 2));
 
   const printedSource = toSource(node);
 
