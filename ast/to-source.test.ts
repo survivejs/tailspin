f4b4ed7495243f0bc6af124da466f4b7fe5e7da8
Juho Vepsäläinen <bebraw@gmail.com>
2020-09-18 11:17:30 +0200

    chore: Add a demo to debug

--- ast/to-source.test.ts
+++ ast/to-source.test.ts
@@ -4,10 +4,10 @@ import toSource from "./to-source.ts";
 
 Deno.test("prints component source", () => {
   const source = "const Demo = () => <div>demo</div>;";
-  const type = "VariableDeclaration";
+  const type = "ArrowFunctionExpression"; // "VariableDeclaration";
   const node = queryNodes({ source, query: { type } })[0];
 
-  // console.log(JSON.stringify(node, null, 2));
+  console.log(JSON.stringify(node, null, 2));
 
   const printedSource = toSource(node);
 
