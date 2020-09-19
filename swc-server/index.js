217017e9edaa0a59cb4ca3ccc0ef8730f0bc5e4e
Juho Vepsäläinen <bebraw@gmail.com>
2020-09-19 17:44:29 +0200

    feat: Add a tiny server to run swc through Node
    The idea is that Deno will use this for now until a better way exists.

---
+++ swc-server/index.js
@@ -0,0 +1,51 @@
+const swc = require('@swc/wasm');
+const Koa = require('koa');
+
+function serve(port) {
+  const app = new Koa();
+ 
+  // response
+  app.use(context => {
+    const { url, query } = context;
+    const urlRoot = url.split('?')[0]
+
+    // http://localhost:4000/parse?source=%22const%20a%20=%20%3Cdiv%3Efoo%3C/div%3E%22
+    if (urlRoot === '/parse') {
+      const { source } = query;
+
+      if (!source) {
+        context.status = 404;
+      }
+      else {
+        context.body = parse(source);
+      }
+    }
+    // http://localhost:4000/parse?ast=%22<AST goes here>%22
+    else if (urlRoot === '/print') {
+      const { ast } = query;
+
+      if (!source) {
+        context.status = 404;
+      }
+      else {
+        context.body = print(ast);
+      }
+    }
+    else {
+      context.status = 404
+    }
+  });
+  
+  console.log('swc-server - running at port 4000')
+  app.listen(port);
+}
+
+function parse(str) {
+  return swc.parseSync("const a = 5;", { syntax: "typescript", tsx: true });
+}
+
+function print(ast) {
+  return swc.printSync(ast, {});
+}
+
+serve(process.env.PORT || 4000);
