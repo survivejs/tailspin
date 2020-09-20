340fc5352f78563890cb9362f3e7d41c66f90def
Juho Vepsäläinen <bebraw@gmail.com>
2020-09-20 15:40:25 +0200

    chore: Drop dead code

--- ast/parse-code.ts
+++ ast/parse-code.ts
@@ -27,50 +27,7 @@ async function parseCode(
     }
   }
 
-  // TODO
-  return Promise.resolve();
-  /*
-  const exampleIdentifierNodes = await queryNodes({
-    source,
-    // query: `Identifier[name="${name}"]`,
-    // TODO
-    query: { type: name },
-  });
-  const exampleIdentifierNode = exampleIdentifierNodes[0];
-
-  if (!exampleIdentifierNodes) {
-    return;
-  }
-
-  const identifierSource = await printAst(
-    // @ts-ignore TODO: Attach parent info
-    exampleIdentifierNode.parent,
-  );
-  let exampleJsxNodes = await queryNodes({
-    source: identifierSource,
-    // query: "JsxElement",
-    // TODO
-    query: {},
-  });
-  let exampleJsxNode = exampleJsxNodes[0];
-
-  if (!exampleJsxNode) {
-    exampleJsxNodes = await queryNodes({
-      source: identifierSource,
-      // query: "JsxSelfClosingElement",
-      // TODO
-      query: {},
-    });
-    exampleJsxNode = exampleJsxNodes[0];
-
-    if (!exampleJsxNode) {
-      console.error("queryNode - No nodes found", { source });
-
-      return;
-    }
-  }
-
-  return printAst(exampleJsxNode);*/
+  return Promise.resolve("");
 }
 
 export default parseCode;
